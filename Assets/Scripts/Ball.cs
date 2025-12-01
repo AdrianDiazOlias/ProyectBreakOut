c09458f1d9f147a332501e7a3b2020933d0cf45d
AdrianDiazOlias <adriart3.0@gmail.com>
2025-12-01 11:36:31 +0100

    Tarea Introducción al Input System

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,5 +1,33 @@
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    Rigidbody rb;
+    public float launchForce = 5f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        if (!GameManager.instance.isGameStarted)
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            if (player != null)
+            {
+                this.transform.SetParent(player.transform);
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            this.transform.SetParent(null);
+            GameManager.instance.isGameStarted = true;
+
+            rb.AddForce(transform.up * launchForce, ForceMode.Impulse);
+        }
+    }
 }
